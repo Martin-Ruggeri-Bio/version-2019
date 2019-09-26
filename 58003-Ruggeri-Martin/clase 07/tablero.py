7164c2869245aff46b6a326611d575851cc768fa
Martin-Ruggeri-Bio <martinruggeri18@gmail.com>
2019-09-26 19:02:18 -0300

    tateti terminado

--- 58003-Ruggeri-Martin/clase 07/tablero.py
+++ 58003-Ruggeri-Martin/clase 07/tablero.py
@@ -1,3 +1,5 @@
+def full(tablero):
+    return not " " in tablero
 def draw_tablero(tablero):
     print(" "+tablero[0]+" | "+tablero[1]+" | "+tablero[2])
     print("---+---+---")
@@ -11,10 +13,10 @@ def win(tablero):
         return True
     if tablero[2]!=" " and tablero[2]==tablero[4] and tablero [4]==tablero[6]:
         return True
-    for i in range (0,3)
+    for i in range (0,3):
         if tablero[i]!=" " and tablero[i]==tablero[i+3] and tablero[i]==tablero[i+6]:
             return True
-    for i in range (0,7,3)
+    for i in range (0,7,3):
         if tablero[i]!=" " and tablero[i]==tablero[i+1] and tablero[i]==tablero[i+2]:
             return True
     return False
--- 58003-Ruggeri-Martin/clase 07/tateti.py
+++ 58003-Ruggeri-Martin/clase 07/tateti.py
@@ -1,3 +1,36 @@
-from tablero import draw_tablero
-tateti==False
-    draw_tablero(["x"," "," "," ","x"," "," "," ","x",])
+from tablero import draw_tablero,full,validate,win
+tablero=[]
+for i in range(0,9):
+    tablero.append(" ")
+    #posicion=int(input())(es el scan)
+draw_tablero(tablero)
+while win(tablero)==False and full(tablero)==False:
+    print("turno x")
+    valido=False
+    while not valido:
+        print("ingresa un posicion del 1 al 9 (X):")
+        posicion=int(input())
+        valido=validate(tablero,posicion)
+        if not valido:
+            print("error de posicion")
+    tablero[posicion-1]="x"
+    draw_tablero(tablero)
+    gano=win(tablero)
+    if gano:
+        print("gano (X)")
+    else:
+        print("turno O")
+        valido=False
+        while not valido:
+            print("ingresa un posicion del 1 al 9 (O):")
+            posicion=int(input())
+            valido=validate(tablero,posicion)
+            if not valido:
+                print("error de posicion")
+        tablero[posicion-1]="O"
+        draw_tablero(tablero)
+        gano=win(tablero)
+        if gano:
+            print("gano (O)")
+if full(tablero) and not win(tablero):
+    print("Nadie gano")
